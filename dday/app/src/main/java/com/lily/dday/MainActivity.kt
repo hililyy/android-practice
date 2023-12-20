cf1e044b8fb563346b6ad80f52e8aed62ff6993d
hililyy <joun406@gmail.com>
2023-12-20 18:13:36 +0900

    [ADD] 디데이 앱 실습

---
+++ dday/app/src/main/java/com/lily/dday/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.lily.dday
+
+import android.app.DatePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.TextView
+import java.util.Calendar
+import java.util.GregorianCalendar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val startButton = findViewById<Button>(R.id.startBtn)
+        val endButton = findViewById<Button>(R.id.endBtn)
+
+        var startDate = ""
+        var endDate = ""
+
+        startButton.setOnClickListener {
+
+            val today = GregorianCalendar()
+            val year = today.get(Calendar.YEAR)
+            val month = today.get(Calendar.MONTH)
+            val day = today.get(Calendar.DATE)
+
+            val dlg = DatePickerDialog(this, object : DatePickerDialog.OnDateSetListener {
+                override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+
+                    startDate = "${year} + ${month + 1} + ${dayOfMonth}"
+                    endDate = "${year} + ${month + 1} + ${dayOfMonth}"
+
+                    val textArea =
+
+                    findViewById<TextView>(R.id.finalDate).setText((endDate.toInt() - startDate.toInt() + 1).toString())
+                }
+
+            }, year, month, day)
+        }
+    }
+}
